af6e05499edf474cedc1b2a6b834237a82f13596
Hussien-LTS <hussain662536@gmail.com>
2023-12-11 01:09:40 +0300

    initial seeding

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,30 +1,27 @@
 import axios from "axios";
 import { PrismaClient } from "@prisma/client";
-import { Country } from "../Interfaces";
 
 const prisma = new PrismaClient();
+const instance = axios.create({
+  timeout: 20000,
+});
 async function seedDatabase() {
   try {
     // Fetch data from the REST Countries API
-    const response = await axios.get<Country[]>(
-      "https://restcountries.com/v3.1/all"
-    );
+    const response = await instance.get("https://restcountries.com/v3.1/all");
     const countries = response.data;
 
-    // Seed the database with the fetched data
     for (const country of countries) {
       await prisma.country.create({
         data: {
-          name: country.name,
-          languages: country.languages,
-          cca2: country.cca2,
-          cca3: country.cca3,
-          ccn3: country.ccn3,
-          currencies: country?.currencies,
-          region: country.region,
-          latlng: country.latlng,
-
-          // Add other fields based on your Prisma schema
+          name: country?.name?.common || country?.name?.official,
+          languages: country.languages || "no value found",
+          cca2: country.cca2 || "no value found",
+          cca3: country.cca3 || "no value found",
+          ccn3: country.ccn3 || "no value found",
+          currencies: country?.currencies || "no value found",
+          region: country.region || "no value found",
+          latlng: country.latlng || "no value found",
         },
       });
     }
@@ -33,7 +30,6 @@ async function seedDatabase() {
   } catch (error) {
     console.error("Error during database seeding:", error);
   } finally {
-    // Disconnect the Prisma client
     await prisma.$disconnect();
   }
 }
