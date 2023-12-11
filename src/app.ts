e9970f02bb80213ced5ea8085f31f57af800706f
Hussien-LTS <hussain662536@gmail.com>
2023-12-11 13:30:09 +0300

    feat/ search for country by CCA2/CCA3/CCN3 completed

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
-// Import necessary modules
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import countriesRouter from "./routes/CountryRoutes";
-// Create an Express application
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// Routes
-
 app.use("/countries", countriesRouter);
-// // Error handling middleware
+
+// Error handling middleware
 // app.use((err, req, res, next) => {
 //   console.error(err.stack);
 //   res.status(500).send("Something went wrong!");
